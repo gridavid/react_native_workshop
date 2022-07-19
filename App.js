a8a60b20e06685d009425fed967afcaf74275d43
hovo505 <hovo.55.k.yan@gmail.com>
2022-07-19 15:21:18 +0400

    tried react native

--- App.js
+++ App.js
@@ -5,6 +5,11 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <View style={{
+        width: 100,
+        height: 100,
+        backgroundColor: 'orange'
+      }}><Text>sfsdfds</Text></View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#dbc8f7',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    width: 100,
+        height: 100,
+        backgroundColor: 'orange',
+        borderRadius:240,
+  }
 });
