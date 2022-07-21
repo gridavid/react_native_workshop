87c51451bb7a67be11cc3272fed0c31adc1145fd
IloveGreeny <94252672+IloveGreeny@users.noreply.github.com>
2022-07-21 15:34:57 +0400

    Update Food.js

--- Components/Food/Food.js
+++ Components/Food/Food.js
@@ -1,1 +1,99 @@
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  ActivityIndicator,
+  FlatList,
+} from "react-native";
+
+
+const movieURL = "https://reactnative.dev/movies.json";
+
+export default function Food(){
+
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [title, setTitle] = useState([]);
+  const [description, setDescription] = useState([]);
+
+
+  useEffect(() => {
+    fetch(movieURL)
+      .then((response) => response.json()) 
+      .then((json) => {
+        setData(json.movies);
+        setTitle(json.title);
+        setDescription(json.description);
+      })
+      .catch((error) => alert(error)) 
+      .finally(() => setLoading(false)); 
+  }, []);
+
+
+  async function getMoviesAsync() {
+    try {
+      let response = await fetch(movieURL);
+      let json = await response.json();
+      setData(json.movies);
+      setTitle(json.title);
+      setDescription(json.description);
+      setLoading(false);
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {}
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <View>
+          {}
+          <Text style={styles.title}>{title}</Text>
+          {}
+          <View style={{ borderBottomWidth: 1, marginBottom: 12 }}></View>
+          <FlatList
+            data={data}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <View style={{ paddingBottom: 10 }}>
+                <Text style={styles.movieText}>
+                  {item.id}. {item.title}, {item.releaseYear} 
+                </Text>
+              </View>
+            )}
+          />
+
+        </View>
+      )}
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 48,
+  },
+  movieText: {
+    fontSize: 26,
+    fontWeight: "200",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  description: {
+    textAlign: "center",
+    marginBottom: 18,
+    fontWeight: "200",
+    color: "green",
+  },
+});
+
 
