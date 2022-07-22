96c04bb1035f2f12df86f1f60b66959924358532
Gri6 <alixanyan.grigor5@gmail.com>
2022-07-22 15:05:38 +0400

    fixed

--- Components/MyStack/MyStack.js
+++ Components/MyStack/MyStack.js
@@ -1,9 +1,5 @@
 import { createStackNavigator } from '@react-navigation/stack';
-import { StatusBar } from 'expo-status-bar';
-import {  View } from 'react-native';
 import Home from '../Home/Home';
-
-
 import Drink from '../Drink/Drink';
 import Receipts from '../Receipts/Receipts';
 import FoodComp from '../Food_alt/FoodComp';
