781bb0d8424e9843c598a89e2add05c7bfab82d9
gridavid <51162620+gridavid@users.noreply.github.com>
2022-07-25 15:16:14 +0400

    Update MyStack.js

--- Components/MyStack/MyStack.js
+++ Components/MyStack/MyStack.js
@@ -1,35 +1,20 @@
 import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet } from 'react-native';
-import Home from '../Home/Home';
-import Food from '../Food/Food';
-
+import { createStackNavigator } from '@react-navigation/stack';
+//import FoodComp from '../Food_alt/FoodComp';
 import Drink from '../Drink/Drink';
 import Receipts from '../Receipts/Receipts';
+import Home from "../Home/Home";
+
 
 const Stack = createStackNavigator();
 
-export default function MyStack() {
-    return (
+export default function MyStack(){
+    return(
         <Stack.Navigator>
-            <Stack.Screen options={header}
-                name="Home"
-                component={Home} />
-            <Stack.Screen options={header} name="Receipts" component={Receipts} />
-            <Stack.Screen options={header} name="Food" component={Food} />
-            <Stack.Screen options={header} name="Drink" component={Drink} />
-
+            <Stack.Screen name = "Home" component = {Home} />
+            <Stack.Screen name = "Receipts" component = {Receipts} />
+            <Stack.Screen name = "Drink" component = {Drink} />
+            
         </Stack.Navigator>
     )
-}
-
-const header = {
-    headerStyle: {
-        backgroundColor: '#f4511e',
-        
-    },
-    headerTitleAlign: 'center',
-    headerTintColor: '#fff',
-    headerTitleStyle: {
-        fontWeight: 'bold',
-    },
 }
