26680e81546a136b9a5083c5be14d463e3e77ff0
gridavid <davit.grigoryan13.y@tumo.org>
2022-07-25 14:20:37 +0400

    sharing mealPlan file

--- Components/MyStack/MyStack.js
+++ Components/MyStack/MyStack.js
@@ -2,7 +2,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../Home/Home';
 import Drink from '../Drink/Drink';
 import Receipts from '../Receipts/Receipts';
-import TempFood from '../TempFood/TempFood';
+
 
 const Stack = createStackNavigator();
 
@@ -11,7 +11,6 @@ export default function MyStack(){
         <Stack.Navigator>
             <Stack.Screen name = "Home" component = {Home} />
             <Stack.Screen name = "Receipts" component = {Receipts} />
-            <Stack.Screen name = "Food" component = {TempFood } />
             <Stack.Screen name = "Drink" component = {Drink} />
             
         </Stack.Navigator>
