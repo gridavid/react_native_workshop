d2eaac3e33e2077225f544f4d7ac3cff3d6128f2
IloveGreeny <94252672+IloveGreeny@users.noreply.github.com>
2022-07-26 14:09:46 +0400

    Update MyStack.js

--- Components/MyStack/MyStack.js
+++ Components/MyStack/MyStack.js
@@ -2,8 +2,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Drink from '../Drink/Drink';
 import Receipts from '../Receipts/Receipts';
 import Home from "../Home/Home";
-import Food from "../Food/Food"
-
+import Food from "../Food/Food";
+import FoodComp from "../Food_alt/FoodComp";
 
 const Stack = createStackNavigator();
 
@@ -14,6 +14,7 @@ export default function MyStack(){
             <Stack.Screen name = "Receipts" component = {Receipts} />
             <Stack.Screen name = "Drink" component = {Drink} />
             <Stack.Screen name = "Food" component = {Food} />
+            <Stack.Screen name = "Meal Plan" component = {FoodComp} />
         </Stack.Navigator>
     )
 }
