b6092fb4eb054c09bd40cfe24e0820f8c23b871a
IloveGreeny <94252672+IloveGreeny@users.noreply.github.com>
2022-07-26 14:11:06 +0400

    Update Food.js

--- Components/Food_alt/Food.js
+++ Components/Food_alt/Food.js
@@ -1,21 +1,10 @@
 import React , { useState , useEffect} from "react";
 import {  View , Text , Image } from 'react-native';
-import { useWindowDimensions } from 'react-native';
-import RenderHtml from 'react-native-render-html';
-import { create } from "xmlbuilder";
 
-
-const renderersProps = {
-  img: {
-    enableExperimentalPercentWidth: true
-  }
-};
 
 
 export default function Food({ meal }) {
   const [imageUrl, setImageUrl] = useState("");
-  const { width } = useWindowDimensions();
-  console.log(imageUrl)
   useEffect(() => {
     fetch(
       `https://api.spoonacular.com/recipes/${meal.id}/information?apiKey=cb1c464d94f142c08b156c5beddade8b&includeNutrition=false`
@@ -32,11 +21,11 @@ export default function Food({ meal }) {
   return (
     <View>
       <Text>{meal.title}</Text>
-      <Image style={{ width : 200, height : 200}} source={imageUrl}/>
+      <Image style={{ width : 200, height : 200}} source={{uri: imageUrl}}/>
       <Text>Preparation time: {meal.readyInMinutes} minutes</Text>
       <Text>Number of servings: {meal.servings}</Text>
-      <Text>Recipe {meal.sourceUrl}</Text>
     </View>
   )
 }
+
 
